024a660036e012bd740a10ee2db5f45163f8d342
carnuare <carnuare@gmail.com>
2022-12-30 01:15:11 +0100

    Users routes mapped

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const passport = require("./passport");
 const authRouter = require("./routes/auth");
 const songsRouter = require("./routes/ms/songs/songs");
 const likesRouter = require("./routes/ms/songs/likes");
+const usersRouter = require("./routes/ms/users/users");
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.use(cookieParser());
 app.use(passport.initialize());
 
 const BASE_PATH_SONGS = "/api/v1";
+const BASE_PATH_USERS = "/api/v1";
 
 app.get("/", (req, res) => {
   res.send('FastMusik API Gateway running!');
@@ -25,5 +27,6 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRouter);
 app.use(`${BASE_PATH_SONGS}/songs`, songsRouter);
 app.use(`${BASE_PATH_SONGS}/likes`, likesRouter);
+app.use(`${BASE_PATH_USERS}/users`, usersRouter);
 
 module.exports = app;
---
+++ routes/ms/users/users.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const passport = require("passport");
+const jwtDecode = require('jwt-decode');
+const APIGateway = require("../../shared/APIGateway");
+
+const router = express.Router();
+const BASE_PATH = process.env.USERS_HOST;
+
+router.get("/", async function (req, res, next) {
+  try {
+    const response = await APIGateway.request({
+      basePath: BASE_PATH,
+      endpoint: req.originalUrl,
+    });
+    res.send(response.data);
+  } catch (error) {
+    res.status(error.response.status).send(error.response.data);
+  }
+});
+
+router.post(
+    "/likes",
+    passport.authenticate("jwt", { session: false }),
+    async function (req, res, next) {
+        try {
+            const decodedToken = jwtDecode(req.headers.authorization);
+            const userId = decodedToken.id;
+            // add the user id to the request body
+            req.body.userId = userId;
+            const response = await APIGateway.request({
+                basePath: BASE_PATH,
+                endpoint: req.originalUrl,
+                method: "POST",
+                data: req.body
+            });
+            res.sendStatus(response.status);
+        } catch (error) {
+            res.status(error.response.status).send(error.response.data);
+        }
+    }
+);
+
+// get user likes
+router.get(
+    "/likes",
+    passport.authenticate("jwt", { session: false }),
+    async function (req, res, next) {
+        try {
+            const decodedToken = jwtDecode(req.headers.authorization);
+            const userId = decodedToken.id;
+            // add the user id to the request body
+            req.body.userId = userId;
+            const response = await APIGateway.request({
+                basePath: BASE_PATH,
+                endpoint: "api/v1/users/likes/all",
+                method: "GET",
+                data: req.body,
+            });
+            res.send(response.data);
+        } catch (error) {
+            res.status(error.response.status).send(error.response.data);
+        }
+    }
+);
+
+router.post(
+    "/profile",
+    passport.authenticate("jwt", { session: false }),
+    async function (req, res, next) {
+      try {
+        // Decode the JWT token
+        const decodedToken = jwtDecode(req.headers.authorization);
+        // Get the id attribute from the decoded token
+        const userId = decodedToken.id;
+        // Use the id attribute in your API request
+        const response = await APIGateway.request({
+          basePath: BASE_PATH,
+          endpoint: `api/v1/users/${userId}`,
+          method: "GET",
+          data: req.body,
+          token: req.headers.authorization,
+        });
+        res.send(response.data);
+      } catch (error) {
+        res.status(error.response.status).send(error.response.data);
+      }
+    }
+  );  
+  
+module.exports = router;
